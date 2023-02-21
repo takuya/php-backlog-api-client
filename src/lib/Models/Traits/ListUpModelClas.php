46772accce032e0315a457db4e3e3e58ce8f1568
takuya <55338+takuya@users.noreply.github.com>
2023-02-22 06:26:02 +0900

    名前空間の変更が__NAMESPACE__参照を壊すので修理。

--- src/lib/Models/Traits/ListUpModelClas.php
+++ src/lib/Models/Traits/ListUpModelClas.php
@@ -4,14 +4,16 @@ namespace Takuya\BacklogApiClient\Models\Traits;
 
 trait ListUpModelClas {
   public static function listModelClass():array{
-    
-    $list = array_merge(glob(__DIR__.'/*.php'),glob(__DIR__.'/**/*.php'));
-    $list = array_map(fn($e)=>str_replace(__DIR__.DIRECTORY_SEPARATOR,'',$e),$list);
+  
+    $ref = new \ReflectionClass(self::class);
+    $dir = dirname($ref->getFileName());
+    $list = array_merge(glob($dir.'/*.php'),glob($dir.'/**/*.php'));
+    $list = array_map(fn($e)=>str_replace($dir.DIRECTORY_SEPARATOR,'',$e),$list);
     $list = array_filter($list,fn($e)=>!str_contains(strtolower($e),'traits'));
-    $list = array_filter($list,fn($e)=>!str_contains(basename($e),basename(__FILE__)));
+    $list = array_filter($list,fn($e)=>!str_contains(basename($e),basename($ref->getFileName())));
     $list = array_map(fn($e)=>str_replace('.php','',$e),$list);
     $list = array_map(fn($e)=>str_replace('/','\\',$e),$list);
-    $list = array_map(fn($e)=>__NAMESPACE__.'\\'.$e,$list);
+    $list = array_map(fn($e)=>$ref->getNamespaceName().'\\'.$e,$list);
     return $list;
   }
   
--- tests/Unit/Model/ModelClassListTest.php
+++ tests/Unit/Model/ModelClassListTest.php
@@ -4,6 +4,8 @@ namespace tests\Unit\Model;
 
 use tests\TestCase;
 use Takuya\BacklogApiClient\Models\BaseModel;
+use Takuya\BacklogApiClient\Models\Issue;
+use Takuya\BacklogApiClient\Models\Project;
 
 class ModelClassListTest extends TestCase {
   public function test_list_declared_class_in_model(){
@@ -13,6 +15,8 @@ class ModelClassListTest extends TestCase {
       $ns = $ref->getNamespaceName();
       $this->assertStringContainsString("Takuya\BacklogApiClient\Models",$ns);
     }
+    $this->assertContains(Issue::class,$models);
+    $this->assertContains(Project::class,$models);
   }
   
 }
