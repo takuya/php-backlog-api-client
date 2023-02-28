93a595be4ee53ff62f38b14a206385f51e2be88f
takuya <55338+takuya@users.noreply.github.com>
2023-02-28 18:50:20 +0900

    relation

--- src/lib/Models/Traits/RelateToSpace.php
+++ src/lib/Models/Traits/RelateToSpace.php
@@ -4,6 +4,7 @@ namespace Takuya\BacklogApiClient\Models\Traits;
 
 trait RelateToSpace {
   protected ?string $space_key;
+  
   public function getSpaceKey (): ?string {
     return $this->space_key;
   }
@@ -12,9 +13,12 @@ trait RelateToSpace {
     $this->space_key = $key;
   }
   
-  public function relation($parent=null):void{
-    parent::relation($parent);
+  public function relation ( $parent = null ): void {
+    parent::relation( $parent );
     $this->space_key = null;
-    $this->space_key = $this->spaceKey ?? $parent?->getSpaceKey() ?? $parent?->spaceKey ?? null;
+    $this->space_key = $parent?->spaceKey ?? null;
+    if ( $parent && method_exists( $parent, 'getSpaceKey' ) ) {
+      $this->space_key = $parent->getSpaceKey() ?? null;
+    }
   }
 }
