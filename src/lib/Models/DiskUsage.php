a94364e20bd2083e26475822a250b0645e996583
takuya <55338+takuya@users.noreply.github.com>
2023-02-28 17:17:22 +0900

    relation

--- src/lib/Models/DiskUsage.php
+++ src/lib/Models/DiskUsage.php
@@ -3,10 +3,12 @@
 namespace Takuya\BacklogApiClient\Models;
 
 use Takuya\BacklogApiClient\Models\Traits\HasProjectId;
+use Takuya\BacklogApiClient\Models\Traits\RelateToSpace;
 
 class DiskUsage extends BaseModel {
   
   use HasProjectId;
+  use RelateToSpace;
   public int $issue;
   public int $wiki;
   public int $file;
