ba394553daa30f3d24bf344a3bd84046dcbc2707
takuya <55338+takuya@users.noreply.github.com>
2023-03-07 17:36:54 +0900

    コピー中の担当者を変えられるようにする。

--- src/lib/Backup/Copy/BacklogCopy.php
+++ src/lib/Backup/Copy/BacklogCopy.php
@@ -9,16 +9,19 @@ use Takuya\BacklogApiClient\Backup\Copy\Traits\CopyIssue;
 class BacklogCopy {
   
   use CopyProject;
-  use CopyIssue;
   
   protected array $id_mapping;
   protected BacklogAPIClient $src_cli;
   protected BacklogAPIClient $dst_cli;
+  protected int $assignee_user_id;
   
   public function __construct ( BacklogAPIClient $api_client,  $dst_space=null, ) {
     $this->src_cli = $api_client;
     $this->dst_cli = $this->dst_space ?? $api_client;
   }
-  
+  public function setAssignee(int $user_id){
+    // 課題をコピーするときに別プロジェクト二ユーザーがいないとき、代わりに使うユーザ
+    $this->assignee_user_id= $user_id;
+  }
   
 }
